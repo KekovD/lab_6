c4ccd7b803ec32854abfd4cc9d2a5270567bda7e
1M0NKEY1 <113592579+1M0NKEY1@users.noreply.github.com>
2022-12-07 18:28:03 +0300

    new update

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 class Calculator:
     def __init__(self):
         pass
+    
+def subtract(self, x1, x2):
+    return x1 - x2
+
+def divide(self, x1, x2):
+    if x2 != 0:
+        return x1 / x2
