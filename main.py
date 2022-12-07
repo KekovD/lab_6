3ca192c41b760cd2650b0f032c4b439ffbcf23d0
KekovD <112900044+KekovD@users.noreply.github.com>
2022-12-07 18:50:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ class Calculator:
     def __init__(self):
         pass
     
-def subtract(self, x1, x2):
-    return x1 - x2
+    def subtract(self, x1, x2):
+        return x1 - x2
 
-def divide(self, x1, x2):
-    if x2 != 0:
-        return x1 / x2
-def add(self, x1, x2):
-    return x1 + x2
-def multiply(self, x1, x2):
-    return x1 * x2
+    def divide(self, x1, x2):
+        if x2 != 0:
+            return x1 / x2
+        
+    def add(self, x1, x2):
+        return x1 + x2
+    
+    def multiply(self, x1, x2):
+        return x1 * x2
